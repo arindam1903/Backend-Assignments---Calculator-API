11ab500d3775327179dd22a9f624cfbabffa18a9
Arindam dutta <arindam408@gmail.com>
2021-02-09 17:17:11 +0530

    change

--- src/index.js
+++ src/index.js
@@ -147,7 +147,7 @@ app.post('/multiply',(req,res)=>{
     let num1=(req.body.num1);
     let num2=req.body.num2;
     let result=parseFloat(num1)/parseFloat(num2);
-  
+  console.log(result);
     if (isNaN(num1)|| isNaN(num2)){
       res.send( {  
         status: "failure",
@@ -163,9 +163,10 @@ app.post('/multiply',(req,res)=>{
         message:"Cannot divide by zero",
         sum: undefined
       });
+      return;
     }
   
-   /* else if (num1>1000000 || num2>1000000 ||result>1000000){
+   else if (num1>1000000 || num2>1000000 ||result>1000000){
        res.send({
          status: "error",
          message:"Overflow",
@@ -181,7 +182,7 @@ app.post('/multiply',(req,res)=>{
         sum: undefined
       });
       return;
-    }*/
+    }
     
     else {
       res.send({
